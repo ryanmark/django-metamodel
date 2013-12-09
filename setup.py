f465f2d2c91c9607a47d5bcb42f3ba7b6649d4f7
Ryan Mark <ryan@mrk.cc>
2013-12-09 14:12:03 -0600

    set version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="django-metamodel",
     packages=find_packages(),
-    version="1.0",
+    version="1.1.0",
     author="Ryan Mark",
     author_email="ryan@mrk.cc",
     description="Add arbitrary data to your django users. This app adds `user.meta` which acts like a dictionary and stores whatever the hell you want. But you could add `.meta` to any of your models.",
